7835cce68dc64c03f9689307db40bce017f90891
Nguyen Long Nhat <27698189+torn4dom4n@users.noreply.github.com>
2026-08-12 10:30:48 +0700

    Update dark mode toggle text to include light mode

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
           <Button className="mt-2">Button</Button>
         </div>
         <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
+          (Press <kbd>d</kbd> to toggle dark/light mode)
         </div>
       </div>
     </div>
